07221afdc5ba2c790e3d2ca94ed8934395b66f9a
wellington.gsantos9 <wellington.gsantos9@ITQ0626035W10-1>
2021-07-08 11:02:07 -0300

    ..

--- PJIntegrador/Classes/Acesso.cs
+++ PJIntegrador/Classes/Acesso.cs
@@ -8,41 +8,38 @@ namespace PJIntegrador.Classes
 {
     public class Acesso
     {
-        public int ID { get; set; }
-        public string Senha { get; set; }
-        public string Tipo { get; set; }
+        public int ID { get; set; } 
         public string Cpf { get; set; }
-        public int IdUsuario { get; set; }
-        public int IdCliente { get; set; }
+        public string Senha { get; set; }
+       
+       
+        
 
         public Acesso()
         {
         }
-        public Acesso(string senha, string tipo, string cpf, int idUsuario, int idCliente)
-        {
-            Senha = senha;
-            Tipo = tipo;
+        public Acesso(string cpf,string senha)
+        { 
             Cpf = cpf;
-            IdUsuario = idUsuario;
-            IdCliente = idCliente;
+            Senha = senha;
+            
+           
+           
         }
         public Acesso(int id,string senha, string tipo, string cpf, int idUsuario, int idCliente)
         {
             this.ID = id;
-            Senha = senha;
-            Tipo = tipo;
             Cpf = cpf;
-            IdUsuario = idUsuario;
-            IdCliente = idCliente;
+            Senha = senha;
+            
+            
+            
         }
-        public void Inserir()
+        public void Logar()
         {
 
         }
-        public void Consultar(int _id)
-        {
-            
-        }
+        
 
 
     }
